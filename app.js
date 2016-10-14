7271b21fcd6fd0c0930256e9c31d97b195866501
Benj Cueto <benj.cueto@music-group.com>
2016-10-14 14:14:41 +0800

    wowjs

--- app.js
+++ app.js
@@ -9,22 +9,46 @@ var FRONTEND = FRONTEND || {};
 
 (function($){
 
-	FRONTEND.productList = {
+	FRONTEND.moduleLoader = {
 		init: function(){
 
-		 	if (!$(".toggle-show-more-list").length) {return;}
-			// dom caching
-			this.masterProduct 	= $('.toggle-show-more-list');
-			this.productList 		= $('.toggle-show-more-lists', this.masterProduct);
-			this.initProductList(this.productList);
+		 // 	if (!$(".toggle-show-more-list").length) {return;}
+			// // dom caching
+			// this.masterProduct 	= $('.toggle-show-more-list');
+			// this.productList 		= $('.toggle-show-more-lists', this.masterProduct);
+			// this.initProductList(this.productList);
+
+			this.wowjs();			
 		},
 
-		initProductList: function(){
-			console.log('success');
+		// initProductList: function(){
+		// 	console.log('success');
 
-		},
+		// },
 
-	}
+		wowjs: function() {
+			var wow = new WOW(
+			  {
+			    boxClass:     'wow',      // animated element css class (default is wow)
+			    animateClass: 'animated', // animation css class (default is animated)
+			    offset:       0,          // distance to the element when triggering the animation (default is 0)
+			    mobile:       true,       // trigger animations on mobile devices (default is true)
+			    live:         true,       // act on asynchronously loaded content (default is true)
+			    callback:     function(box) {
+			      // the callback is fired every time an animation is started
+			      // the argument that is passed in is the DOM node being animated
+			    },
+			    scrollContainer: null // optional scroll container selector, otherwise use window
+			  }
+			);
+			wow.init();
+		}
+
+	};
+
+	$(function(){
+		FRONTEND.moduleLoader.init();
+	});
 
 
 })(jQuery);
